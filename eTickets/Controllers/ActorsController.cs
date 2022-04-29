057addff338d83de6084230486e43c27ba769a59
vastamaa <vsztom@gmail.com>
2022-04-29 16:08:03 +0200

    Added controllers: actors, movies, cinemas, producers

---
+++ eTickets/Controllers/ActorsController.cs
@@ -0,0 +1,16 @@
+﻿using eTickets.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace eTickets.Controllers
+{
+    public class ActorsController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ActorsController(AppDbContext context) => _context = context;
+
+        public async Task<IActionResult> Index() => View(await _context.Actors.ToListAsync());
+    }
+}
