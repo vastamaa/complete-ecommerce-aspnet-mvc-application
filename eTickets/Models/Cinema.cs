1dc9b100776cbd47dfd9e7714b92b628da96dd36
vastamaa <vsztom@gmail.com>
2022-04-29 19:48:30 +0200

    Added Cinemas View

--- eTickets/Models/Cinema.cs
+++ eTickets/Models/Cinema.cs
@@ -1,12 +1,16 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace eTickets.Models
 {
     public class Cinema
     {
         public int Id { get; set; }
+        [Display(Name = "Logo")]
         public string Logo { get; set; }
+        [Display(Name = "Name")]
         public string Name { get; set; }
+        [Display(Name = "Description")]
         public string Description { get; set; }
 
         //Relationships
