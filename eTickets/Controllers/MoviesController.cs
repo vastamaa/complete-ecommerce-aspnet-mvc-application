a54e425ce07c44b145a387f86da008926233b6b2
vastamaa <vsztom@gmail.com>
2022-04-30 19:58:28 +0200

    Copied already done Movie Cards structure from course repository

--- eTickets/Controllers/MoviesController.cs
+++ eTickets/Controllers/MoviesController.cs
@@ -1,6 +1,7 @@
 ﻿using eTickets.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace eTickets.Controllers
@@ -11,6 +12,9 @@ namespace eTickets.Controllers
 
         public MoviesController(AppDbContext context) => _context = context;
 
-        public async Task<IActionResult> Index() => View(await _context.Movies.ToListAsync());
+        public async Task<IActionResult> Index() => View(await _context.Movies
+            .Include(m => m.Cinema)
+            .OrderBy(n => n.Name)
+            .ToListAsync());
     }
 }
