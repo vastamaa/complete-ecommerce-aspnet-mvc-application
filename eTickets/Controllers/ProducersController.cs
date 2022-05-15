9120dde9cbdfb75af79f58a2a9026048589c6a76
vastamaa <vsztom@gmail.com>
2022-05-15 12:38:35 +0200

    Added producer Update and Delete

--- eTickets/Controllers/ProducersController.cs
+++ eTickets/Controllers/ProducersController.cs
@@ -36,5 +36,48 @@ namespace eTickets.Controllers
             await _producersService.AddAsync(producer);
             return RedirectToAction(nameof(Index));
         }
+
+        //GET: producers/edit/1
+        public async Task<IActionResult> Edit(int id)
+        {
+            var producerDetails = await _producersService.GetByIdAsync(id);
+
+            if (producerDetails is null) return View("NotFound");
+
+            return View(producerDetails);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, [Bind("Id, ProfilePictureURL, FullName, Biography")] Producer producer)
+        {
+            if (!ModelState.IsValid) return View(producer);
+
+            if (id == producer.Id)
+            {
+                await _producersService.UpdateAsync(id, producer);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(producer);
+        }
+
+        //DELETE: producers/delete/1
+        public async Task<IActionResult> Delete(int id)
+        {
+            var producerDetails = await _producersService.GetByIdAsync(id);
+
+            if (producerDetails is null) return View("Not found!");
+
+            return View(producerDetails);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var actorDetails = await _producersService.GetByIdAsync(id);
+            if (!ModelState.IsValid) return View("Not found!");
+
+            await _producersService.DeleteAsync(id);
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
