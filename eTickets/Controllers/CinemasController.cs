9b3327f1fbb7a93a84827c8900a5579a3fb44269
vastamaa <vsztom@gmail.com>
2022-05-15 13:09:41 +0200

    Added Cinema Update

--- eTickets/Controllers/CinemasController.cs
+++ eTickets/Controllers/CinemasController.cs
@@ -32,5 +32,24 @@ namespace eTickets.Controllers
             if (cinemaDetails == null) return View("NotFound");
             return View(cinemaDetails);
         }
+
+        //UPDATE: cinemas/Edit/1
+        public async Task<IActionResult> Edit(int id)
+        {
+            var cinemaDetails = await _cinemasService.GetByIdAsync(id);
+
+            if (cinemaDetails is null) return View("Not found!");
+
+            return View(cinemaDetails);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, [Bind("Id, Logo, Name, Description")] Cinema cinema)
+        {
+            if (!ModelState.IsValid) return View(cinema);
+
+            await _cinemasService.UpdateAsync(id, cinema);
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
